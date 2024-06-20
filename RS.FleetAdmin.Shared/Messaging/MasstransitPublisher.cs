9a1315e504e89ada3a5f4dcb3785493b53b610c4
Bernt Anker <bernt.anker@gmail.com>
2024-06-20 08:10:58 +0200

    Clean arch work in progress

---
+++ RS.FleetAdmin.Shared/Messaging/MasstransitPublisher.cs
@@ -0,0 +1,18 @@
+﻿using MassTransit;
+
+namespace RS.FleetAdmin.Shared.Messaging;
+
+public class MasstransitPublisher<T>
+{
+    private readonly IPublishEndpoint _publishEndpoint;
+
+    public MasstransitPublisher(IPublishEndpoint publishEndpoint)
+    {
+        _publishEndpoint = publishEndpoint;
+    }
+
+    public async Task Publish(T message)
+    {
+        await _publishEndpoint.Publish(message);
+    }
+}
